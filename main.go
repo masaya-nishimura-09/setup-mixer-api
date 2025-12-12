c26002bc936530ff3b0025f6fec57e324908cd1f
Masaya <masaya.nishimura0927@gmail.com>
2025-12-12 10:05:51 +0900

    add a function to search all items

--- main.go
+++ main.go
@@ -1,115 +1,135 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"net/url"
-	"os"
-	"strconv"
+    "encoding/json"
+    "fmt"
+    "io"
+    "net/http"
+    "net/url"
+    "os"
+    "strconv"
+    "strings"
 
-	"github.com/gin-gonic/gin"
-	"github.com/joho/godotenv"
+    "github.com/gin-gonic/gin"
+    "github.com/joho/godotenv"
 )
 
+type Response struct {
+    Desk []Item 
+    Chair []Item 
+    Keyboard []Item 
+    Mouse []Item 
+    MousePad []Item 
+    Monitor []Item 
+    MonitorArm []Item 
+    DeskLamp []Item 
+    PowerStrip []Item 
+    PowerStripCase []Item 
+    Speaker []Item 
+    Microphone []Item 
+    Camera []Item 
+    Headphone []Item 
+}
+
 type SearchResult struct {
-	GenreInformation []int  `json:"GenreInformation"`
-	Items            []Item `json:"Items"`
-	TagInformation   []int  `json:"TagInformation"`
-	Carrier          int    `json:"carrier"`
-	Count            int    `json:"count"`
-	First            int    `json:"first"`
-	Hits             int    `json:"hits"`
-	Last             int    `json:"last"`
-	Page             int    `json:"page"`
-	PageCount        int    `json:"pageCount"`
+    GenreInformation []int  `json:"GenreInformation"`
+    Items            []Item `json:"Items"`
+    TagInformation   []int  `json:"TagInformation"`
+    Carrier          int    `json:"carrier"`
+    Count            int    `json:"count"`
+    First            int    `json:"first"`
+    Hits             int    `json:"hits"`
+    Last             int    `json:"last"`
+    Page             int    `json:"page"`
+    PageCount        int    `json:"pageCount"`
 }
 
 type Item struct {
-	Availability    int      `json:"availability"`
-	Catchcopy       string   `json:"catchcopy"`
-	ItemCaption     string   `json:"itemCaption"`
-	ItemCode        string   `json:"itemCode"`
-	ItemName        string   `json:"itemName"`
-	ItemPrice       int      `json:"itemPrice"`
-	ItemUrl         string   `json:"itemUrl"`
-	MediumImageUrls []string `json:"mediumImageUrls"`
-	ReviewAverage   float64  `json:"reviewAverage"`
-	ReviewCount     int      `json:"reviewCount"`
-	ShopName        string   `json:"shopName"`
-	ShopCode        string   `json:"shopCode"`
+    Availability    int      `json:"availability"`
+    Catchcopy       string   `json:"catchcopy"`
+    ItemCaption     string   `json:"itemCaption"`
+    ItemCode        string   `json:"itemCode"`
+    ItemName        string   `json:"itemName"`
+    ItemPrice       int      `json:"itemPrice"`
+    ItemUrl         string   `json:"itemUrl"`
+    MediumImageUrls []string `json:"mediumImageUrls"`
+    ReviewAverage   float64  `json:"reviewAverage"`
+    ReviewCount     int      `json:"reviewCount"`
+    ShopName        string   `json:"shopName"`
+    ShopCode        string   `json:"shopCode"`
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/items", getItems)
-	router.Run("localhost:8080")
+    err := godotenv.Load()
+    if err != nil {
+        fmt.Println(err)
+    }
+
+    router := gin.Default()
+    router.GET("/items", getItems)
+    router.Run("localhost:8080")
 }
 
-func searchDesk(keywords string) ([]Item, error) {
-	err := godotenv.Load()
-	if err != nil {
-		return nil, fmt.Errorf("fail to load .env file: %w", err)
-	}
+func searchItems(genreId int, keywords string) []Item {
+    baseURL := "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601"
+    u, err := url.Parse(baseURL)
+    if err != nil {
+        fmt.Println(err)
+        return []Item{}
+    }
+    values := u.Query()
+    values.Add("format", "json")
+    values.Add("sort", "-reviewAverage")
+    values.Add("carrier", "0")
+    values.Add("availability", "1")
+    values.Add("imageFlag", "1")
+    values.Add("formatVersion", "2")
+    values.Add("applicationId", os.Getenv("APPLICATION_ID"))
+    values.Add("keyword", keywords)
+    values.Set("genreId", strconv.Itoa(genreId))
+    u.RawQuery = values.Encode()
 
-	baseURL := "https://app.rakuten.co.jp/services/api/IchibaItem/Search/20220601"
-	u, err := url.Parse(baseURL)
-	if err != nil {
-		return nil, fmt.Errorf("fail to parse url: %w", err)
-	}
-	values := u.Query()
-	values.Add("format", "json")
-	values.Add("sort", "-reviewAverage")
-	values.Add("carrier", "0")
-	values.Add("availability", "1")
-	values.Add("imageFlag", "1")
-	values.Add("formatVersion", "2")
-	values.Add("applicationId", os.Getenv("APPLICATION_ID"))
-	values.Add("keyword", keywords)
+    req, err := http.NewRequest(http.MethodGet, u.String(), nil)
+    if err != nil {
+        fmt.Println(err)
+    }
 
-	deskGenreId := [3]int{215698, 215702, 215706}
+    client := new(http.Client)
+    res, err := client.Do(req)
+    if err != nil {
+        fmt.Println(err)
+    }
+    defer res.Body.Close()
 
-	items := []Item{}
+    body, _ := io.ReadAll(res.Body)
 
-	for i := 0; i < len(deskGenreId); i++ {
-		values.Set("genreId", strconv.Itoa(deskGenreId[i]))
-		u.RawQuery = values.Encode()
+    var searchResult SearchResult
+    if err := json.Unmarshal(body, &searchResult); err != nil {
+        fmt.Println(err)
+    }
 
-		req, _ := http.NewRequest(http.MethodGet, u.String(), nil)
-
-		client := new(http.Client)
-		res, err := client.Do(req)
-		if err != nil {
-			return nil, fmt.Errorf("fail to access Rakuten api: %w", err)
-		}
-		defer res.Body.Close()
-
-		body, _ := io.ReadAll(res.Body)
-
-		var searchResult SearchResult
-		if err := json.Unmarshal(body, &searchResult); err != nil {
-			return nil, fmt.Errorf("fail to unmarshal json: %w", err)
-		}
-		items = append(items, searchResult.Items...)
-	}
-
-	return items, nil
+    return searchResult.Items
 }
 
 func getItems(c *gin.Context) {
-	keywords := c.Query("keyword")
+    keywordsArray := c.QueryArray("keyword")
+    keywords := strings.Join(keywordsArray, " ")
 
-	result, err := searchDesk(keywords)
-	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
-		return
-	}
+    var response Response
+    response.Desk = searchItems(215698, keywords)
+    response.Chair = searchItems(111363, keywords)
+    response.Keyboard = searchItems(560088, keywords)
+    response.Mouse = searchItems(565170, keywords)
+    response.MousePad = searchItems(552391, keywords)
+    response.Monitor = searchItems(110105, keywords)
+    response.MonitorArm = searchItems(566221, keywords)
+    response.DeskLamp = searchItems(500281, keywords)
+    response.PowerStrip = searchItems(552481, keywords)
+    response.PowerStripCase = searchItems(200166, keywords)
+    response.Speaker = searchItems(208316, keywords)
+    response.Microphone = searchItems(406336, keywords)
+    response.Camera = searchItems(403512, keywords)
+    response.Headphone = searchItems(568359, keywords)
 
-	if len(result) == 0 {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "item not found"})
-		return
-	}
-
-	c.IndentedJSON(http.StatusOK, result)
+    c.IndentedJSON(http.StatusOK, response)
 }
